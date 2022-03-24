0c917458b08f77bdb541034bf4f68ebc482b2d03
ardilalukita14 <lukitasari623@gmail.com>
2022-03-24 23:49:00 +0700

    Membuat Menu Data User

---
+++ src/components/admin/User.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const daftarUser = (props) => {
+    return (
+            <tr>
+                <td><img src={props.gambar} alt="GambarUser"/></td>
+                <td>{props.nama}</td>
+                <td>{props.nomor_telp}</td>
+                <td>{props.email}</td>
+                <td>{props.password}</td>
+                <td>{props.alamat}</td>
+                <td>
+                <button className="btn btn-sm btn-danger" onClick={() => props.hapusDataUser(props.idUser)}>Hapus</button>      
+                </td>
+                <td>      
+                <button className="btn btn-sm btn-success" onClick={() => props.hapusDataUser(props.idUser)}>Edit</button>
+                </td>
+                </tr>
+              
+          
+    )
+}
+
+export default daftarUser;
