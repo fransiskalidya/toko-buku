02705154be6d5c1bdced786a8de45e4c6da528b5
muhammadakbarazmi <amikpank@gmail.com>
2022-03-27 14:40:11 +0700

    Menambahkan tampilan dashboard

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import image from './booktown.jpg';
 
 const Dashboard=()=>{
     return(
-        <h2>Dashboard</h2>
+        <div>
+        <h1>Selamat Datang Di BookTown</h1>
+        <h3>Toko buku Online terbesar dan terlengkap di Indonesia, dengan harga yang murah dari toko buku lainnya.</h3>
+        <img src={image} height={1000} width={1500}>
+
+        </img>
+    
+        </div>
     )
 }
 
