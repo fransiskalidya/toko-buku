53a880a0894129659c77ac350f8cc4abad482318
muhammadakbarazmi <amikpank@gmail.com>
2022-04-04 22:00:56 +0700

    Update CRUD Kategori

--- src/container/admin/Kategori.jsx
+++ src/container/admin/Kategori.jsx
@@ -1,26 +1,181 @@
 import React, { Component } from "react";
 // import './Kategori.css'
 import DataKategori from '../../components/admin/DataKategori';
+import Alert from '../../components/Alert';
 import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 
 class Kategori extends Component {
-  state = {
-    listKategori: [],
-    insertKategori: {
-      id: 1,
-      nama: "Novel"
+  constructor(props) {
+    super(props);
+    this.state = {
+        listKategori: [],       // Untuk tampung Get all data
+        totalData: 0,       // Untuk Hitung All Data
+        isUpdate: false,    // Untuk Fileter Fungsi 
+        Notif: {            // Untuk Tampung respon Dari Server
+            alertShow: false,
+            actionType: '',
+            responCode: 0,
+        },
+        insertKategori: {      // untuk Tampung data Update / New data
+            id: 1,
+            nama: ""
+        }
     }
-  }
 
-  componentDidMount() {
-    fetch('http://localhost:3001/kategori')
-      .then(response => response.json())
-      .then(jsonHasilAmbilDariAPI => {
-        this.setState({
-          listKategori: jsonHasilAmbilDariAPI
+}
+
+  ambilDataDariServerAPI = () => {
+    fetch('http://localhost:3001/kategori')  // alamat URL API yang ingin kita ambil datanya
+        .then(response => response.json())  // ubah response data dari URL API menjadi sebuah data json
+        .then(jsonHasilAmbilDariAPI => {    // data json hasil ambil dari API kita masukkan ke dalam listUserpada state 
+            this.setState({
+                listKategori: jsonHasilAmbilDariAPI,
+                totalData: jsonHasilAmbilDariAPI.length
+            })
         })
-      })
+}
+
+componentDidMount() {
+    this.ambilDataDariServerAPI()
+
+}
+
+  SaveNewDataKategori = () => {        // fungsi untuk meng-handle tombol simpan
+    fetch('http://localhost:3001/kategori', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify(this.state.insertKategori)      // kirimkan ke body request untuk data Useryang akan ditambahkan (insert)
+    })
+        .then((Response) => {
+            console.log(Response)
+            console.log("Status Create", Response.status)
+            this.setState({
+                Notif: {
+                    alertShow: true,
+                    actionType: 'created',
+                    responCode: Response.status,
+                }
+            })
+
+            this.ambilDataDariServerAPI();      // reload / refresh data
+            this.ClearForm();
+        });
+}
+
+EditDataKategori = () => {
+  const dataUpdate = this.state.insertKategori;
+  const id = dataUpdate.id;
+  fetch('http://localhost:3001/kategori/' + id, {
+      method: 'PUT',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataUpdate)
+  })
+      .then((Response) => {
+          console.log(Response)
+          console.log("Status Update", Response.status)
+          this.setState({
+              Notif: {
+                  alertShow: true,
+                  actionType: 'updated',
+                  responCode: Response.status,
+              }
+          })
+
+          this.ambilDataDariServerAPI();      // reload / refresh data
+          this.ClearForm();
+      });
+}
+
+
+
+HapusDataKategori = (data) => {
+  const id = data;
+
+  fetch('http://localhost:3001/kategori/' + id, {
+      method: 'DELETE'
+  })  // alamat URL API yang ingin kita HAPUS datanya
+      .then(Response => {      // ketika proses hapus berhasil, maka ambil data dari server API lokal 
+          console.log(Response)
+          console.log("Status Delete", Response.status)
+
+          // Untuk Tampung respon Dari Server
+          this.setState({
+              Notif: {
+                  alertShow: true,
+                  actionType: 'deleted',
+                  responCode: Response.status,
+              }
+          })
+          this.ambilDataDariServerAPI();
+          this.ClearForm();
+      });
+}
+
+handleChange = (event) => {      // fungsi untuk meng-handle form tambah data User
+  const NumberingId = this.state.totalData + 1;
+  let formInsertKategori = { ...this.state.insertKategori };
+  if (!this.state.isUpdate) { //Cek Jika Update Data Idnya Tidak Di Ubah
+      formInsertKategori['id'] = NumberingId;
   }
+  formInsertKategori[event.target.name] = event.target.value;      // menyimpan data onchange ke formInsertUsersesuai dengan target yang diisi
+  this.setState({
+      insertKategori: formInsertKategori
+  });
+}
+
+ClearForm = () => {
+
+  this.setState({
+      isUpdate: false,
+      insertKategori: {
+          id: 1,
+          nama: "",
+      }
+  })
+  // Mengembalikan Nilai Awal Notif
+  setInterval(() => {
+      this.setState({
+          Notif: {
+              alertShow: false,
+              actionType: '',
+              responCode: 0,
+          }
+      })
+  }, 4500);
+}
+
+handleSaveButton = () => {
+  if (this.state.isUpdate) {
+      this.EditDataKategori();
+  } else {
+      this.SaveNewDataKategori();
+  }
+}
+
+handleEditKategori = (data) => {
+  console.log('Update id', data.id);
+  console.log('Update arry', data);
+  this.setState({
+      insertKategori: data,
+      isUpdate: true
+  })
+}
+
+handleHapusKategori = (data) => {
+  console.log('Id delete =', data)
+  const id = data;
+
+  if (window.confirm('Apakah data ' + id + ' ?')) {
+      this.HapusDataKategori(id)
+  }
+}
 
   render() {
     return (
@@ -36,12 +191,13 @@ class Kategori extends Component {
                 <img src="https://img.jakpost.net/c/2020/04/21/2020_04_21_93387_1587459137._large.jpg" class="card-img-top" alt="bookStore"></img>
                 <div className="card-body">
                   <form>
+                  <Alert data={this.state.Notif} />
                     <div className="form-floating mb-3">
                       <input disabled className="form-control" id="id" name="id" placeholder="ID" />
                       <label htmlFor="id" for="id">ID</label>
                     </div>
                     <div className="form-floating mb-3">
-                      <input className="form-control" id="nama_kategori" name="nama_kategori" type="text" placeholder="Kategori" />
+                      <input className="form-control" id="nama" name="nama" type="text" placeholder="Kategori"  onChange={this.handleChange} value={this.state.insertKategori.nama}/>
                       <label htmlFor="nama_kategori" for="inputPassword">Kategori</label>
                     </div>
                     <div className="d-flex align-items-center justify-content-between mt-4 mb-0">
@@ -86,8 +242,12 @@ class Kategori extends Component {
 
                 {
                   this.state.listKategori.map(kategori => {  // looping dan masukkan untuk setiap data yang ada di listBuku ke variabel Buku
-                    return <DataKategori key={kategori.id} idKategori={kategori.id} nama={kategori.nama} />
-                  })
+                    return (
+                      <DataKategori key={kategori.id}
+                          data={kategori}
+                          hapusDataKategori={this.handleHapusKategori} EditDataKategori={this.handleEditKategori} />     // mappingkan data json dari API sesuai dengan kategorinya
+                  )
+              })
                 }
               </tbody>
             </table>
