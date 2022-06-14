c6e60ee374dd06c7c07c66c818bb4b7680ce9a1f
ardilalukita14 <lukitasari623@gmail.com>
2022-06-14 23:13:02 +0700

    Logout

--- src/components/frontend/auth/Navbar.jsx
+++ src/components/frontend/auth/Navbar.jsx
@@ -1,33 +1,61 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import firebase from "../../../firebase.config";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import logo from '../../../images/ecommerce.svg';
+import './navbar.css'
+// import { Icon } from 'react-icons-kit'
+// import { cart } from 'react-icons-kit/entypo/cart'
 
-const Navbar=()=>{
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container" style={{border:"none"}}>
-          <Link className="navbar-brand" to={"/login"}>BookTown</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/logincustomer"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/registercustomer"}>Register</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/keranjang"}>Keranjang</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/checkout"}>Checkout</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/akun"}>Akun</Link>
-              </li>
-            </ul>
-          </div>
+import { useHistory } from 'react-router-dom'
+
+
+const Navbar = ({ user }) => {
+
+  const auth = getAuth();
+  const history = useHistory();
+  // handle logout
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      history.push('/logincustomer');
+    })
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+      <div className="container" style={{ border: "none" }}>
+        <Link className="navbar-brand" to={"/login"}>BookTown</Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/logincustomer"}>Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/registercustomer"}>Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/keranjang"}>Keranjang</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/checkout"}>Checkout</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/akun"}>Akun</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/login"}>Admin</Link>
+            </li>
+            <li>
+                <span>
+                <div className="btn-post">
+                  <button className='btn btn-success btn-md mybtn' onClick={handleLogout}>Logout</button>
+                </div></span>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
